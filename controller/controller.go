73c3249b0a4b23bd37664d39cf1c28f0d0318d19
imaishiuta <shuta25le0n@gmail.com>
2019-02-02 23:24:26 +0900

    edit controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import(
+  "fmt"
+  "../session"
+  "../data"
   "github.com/gin-gonic/gin"
 )
 
@@ -13,5 +16,41 @@ func UserRouter(c *gin.Context) {
 }
 
 func ChatRouter(c *gin.Context) {
+  messages, current_user := data.Find_Another_User_Messages(c)
+  fmt.Println(current_user.ID)
+  c.HTML(200, "chat.html", gin.H{
+    "messages": messages,
+    "current_user": current_user,
+    })
+}
+
+func SignupRouter(c *gin.Context) {
+  c.HTML(200, "signup.html", nil)
+}
+
+func PostSignupRouter(c *gin.Context) {
+  name := c.PostForm("name")
+  email := c.PostForm("email")
+  password := c.PostForm("password")
+  confirm_password := c.PostForm("confirm_password")
+  data.Create_User(name, email, password, confirm_password)
+  c.HTML(200, "signin.html", nil)
+}
+
+func SigninRouter(c *gin.Context) {
+  c.HTML(200, "signin.html", nil)
+}
+
+func PostSigninRouter(c *gin.Context) {
+  email := c.PostForm("email")
+  password := c.PostForm("password")
+  user := data.Find_User(email, password)
+  session.Login(c, user)
+  c.HTML(200, "chat.html", nil)
+}
+
+func PostMessage(c *gin.Context) {
+  text := c.PostForm("text")
+  data.Create_Message(c, text)
   c.HTML(200, "chat.html", nil)
 }
